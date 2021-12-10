bc463e1c59531ff23671338df7a49dff67590228
Aravinth <aravinth@Aravinths-MacBook-Air.local>
2021-12-10 19:14:59 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GradeComponent } from './grade/grade.component';
+import { SubjectComponent } from './subject/subject.component';
+import { SubmissionComponent } from './submission/submission.component';    
+import {​ RouterModule }​ from'@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GradeComponent,
+    SubjectComponent,
+    SubmissionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: 'grade', component: GradeComponent},    
+      {path: 'subject', component: SubjectComponent},    
+      {path: 'submission', component: SubmissionComponent},                           
+                       
+                       
+        
+
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
